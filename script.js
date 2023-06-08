9099c3d6d634b1625157c9473e759b707f4a3a15
deeptigupta222 <deeptig22@navgurukul.org>
2023-06-08 14:55:39 +0530

    loops in js

---
+++ script.js
@@ -0,0 +1,36 @@
+// for (let index = 0; index<5;index++){
+//     console.log("deep",index);
+// }
+
+// let a=0;
+// while (a<5){
+//     console.log("a",a);
+//     a+=1;
+// }
+
+// let step=6;
+// do{
+//     console.log("step",step);
+//     step+=1;
+// }while(step<5);
+
+
+
+// let a=0;
+// while (a<5){
+//     console.log("a",a);
+//     a+=1;
+//     if (a===2){
+//         break;
+//     }
+// }
+
+let a=0;
+while (a<5){
+    a+=1;
+    if (a===2){
+        continue;
+    }
+    console.log("a",a);
+   
+}
